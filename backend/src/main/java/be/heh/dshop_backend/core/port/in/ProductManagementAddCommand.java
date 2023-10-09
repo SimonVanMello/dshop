793dab69f2c08bbcaacb0ebe55e547a5deb4de86
SimonVanMello <simon.vanmello@proton.me>
2023-10-09 14:38:10 +0200

    ProductManagemementaddCommand class done

--- backend/src/main/java/be/heh/dshop_backend/core/port/in/ProductManagementAddCommand.java
+++ backend/src/main/java/be/heh/dshop_backend/core/port/in/ProductManagementAddCommand.java
@@ -1,6 +1,7 @@
 package be.heh.dshop_backend.core.port.in;
 
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
@@ -8,16 +9,20 @@ import static be.heh.dshop_backend.common.Validation.validate;
 
 public class ProductManagementAddCommand {
     @Getter
-    @NotNull private final String name;
+    @NotEmpty
+    private final String name;
 
     @Getter
-    @NotNull @Min(0) private final double price;
+    @NotNull @Min(0)
+    private final double price;
 
     @Getter
-    @NotNull @Min(1) private final int quantity;
+    @NotNull @Min(1)
+    private final int quantity;
 
     @Getter
-    @NotNull private final byte[] image;
+    @NotEmpty
+    private final byte[] image;
 
     public ProductManagementAddCommand(String name, double price, int quantity, byte[] image){
         this.name = name;
