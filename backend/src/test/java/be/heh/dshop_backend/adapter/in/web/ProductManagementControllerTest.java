dfe56b1b938dd0e477eb733794c770d025e8766d
SimonVanMello <simon.vanmello@proton.me>
2023-11-29 14:28:12 +0100

    Small changes to unit test

--- backend/src/test/java/be/heh/dshop_backend/adapter/in/web/ProductManagementControllerTest.java
+++ backend/src/test/java/be/heh/dshop_backend/adapter/in/web/ProductManagementControllerTest.java
@@ -2,47 +2,40 @@ package be.heh.dshop_backend.adapter.in.web;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.mock.web.MockPart;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @SpringBootTest
-//@WebMvcTest(controllers = ProductManagementController.class)
 public class ProductManagementControllerTest {
     @Autowired
     private WebApplicationContext webApplicationContext;
 
     @Test
     public void addNewProductReturns201() throws Exception{
-        MockPart name = new MockPart("name", "productName".getBytes());
-        MockPart price = new MockPart("price", "12.4".getBytes());
-        MockPart quantity = new MockPart("quantity", "3".getBytes());
         MockMultipartFile file
                 = new MockMultipartFile(
-                "file",
+                "img",
                 "image.png",
                 MediaType.IMAGE_PNG_VALUE,
                 "fakeImage".getBytes()
         );
 
-        MockMvc mockMvc
-                = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+        MockMvc mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
         mockMvc.perform(multipart("/product")
                 .file(file)
-                .part(name, price, quantity)
+                .param("name", "productName")
+                .param("price", "12.4")
+                .param("quantity", "3")
                 .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+                .andExpect(status().is(201));
 
-        // TODO: find out why this returns 400
     }
 }
