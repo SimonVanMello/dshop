a0401b4a0b0c438208f86d5799bdda58dc3be5d6
SimonVanMello <simon.vanmello@proton.me>
2023-12-15 10:36:11 +0100

    feat: added react router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,5 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import HomePage from "./pages/HomePage/HomePage";
 import GlobalContextProvider from "./store/GlobalContextProvider";
 import AddProductPage from "./pages/AddProductPage/AddProductPage";
@@ -6,9 +8,13 @@ import NavigationBar from "./components/NavigationBar/NavigationBar";
 const App = () => {
 	return (
 		<GlobalContextProvider>
-			<NavigationBar />
-			<AddProductPage />
-			{/* <HomePage /> */}
+			<BrowserRouter>
+				<NavigationBar />
+				<Routes>
+					<Route path='/' element={<HomePage />} exact />
+					<Route path='/newproduct' element={<AddProductPage />} exact />
+				</Routes>
+			</BrowserRouter>
 		</GlobalContextProvider>
 	);
 }
--- frontend/src/components/NavigationBar/NavigationBar.tsx
+++ frontend/src/components/NavigationBar/NavigationBar.tsx
@@ -1,16 +1,24 @@
+import { Link } from 'react-router-dom';
+
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 const NavigationBar = () => {
 	return (
     	<Navbar bg="light" data-bs-theme="light" id="NavBarContainer">
 			<Container>
-			<Navbar.Brand href="#home">Dshop</Navbar.Brand>
+			<Navbar.Brand as={Link} to="/">Dshop</Navbar.Brand>
 				<Nav className="me-auto">
-					<Nav.Link href="#home">Home</Nav.Link>
-					<Nav.Link href="#features">Features</Nav.Link>
-					<Nav.Link href="#pricing">Pricing</Nav.Link>
+					<Nav.Link as={Link} to="/">Home</Nav.Link>
+					<Nav.Link as={Link} to="/">Features</Nav.Link>
+					<Nav.Link as={Link} to="/">Pricing</Nav.Link>
+					<NavDropdown title="Admin" id="basic-nav-dropdown">
+						<NavDropdown.Item as={Link} to="/newproduct">
+							Add new product
+						</NavDropdown.Item>
+					</NavDropdown>
 				</Nav>
 			</Container>
       </Navbar>
