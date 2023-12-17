ad42e03914578a71a57e5ab4a487781e70b736f1
SimonVanMello <simon.vanmello@proton.me>
2023-12-17 15:04:21 +0100

    refactor: updated dependency injection configuration

--- backend/src/main/java/be/heh/dshop_backend/DshopapplicationConfiguration.java
+++ backend/src/main/java/be/heh/dshop_backend/DshopapplicationConfiguration.java
@@ -8,6 +8,10 @@ import be.heh.dshop_backend.core.domain.service.ProductManagementService;
 import be.heh.dshop_backend.core.port.in.GetProductUseCase;
 import be.heh.dshop_backend.core.port.in.GetProductsUseCase;
 import be.heh.dshop_backend.core.port.in.ProductManagementUseCase;
+import be.heh.dshop_backend.core.port.out.GetProductPersistenceOut;
+import be.heh.dshop_backend.core.port.out.GetProductsPersistenceOut;
+import be.heh.dshop_backend.core.port.out.ProductManagementCloudinaryOut;
+import be.heh.dshop_backend.core.port.out.ProductManagementPersistenceOut;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -24,24 +28,43 @@ public class DshopapplicationConfiguration {
     private GetProductRepository getProductRepository;
 
     @Bean
-    public ProductManagementUseCase getProductManagementUseCase(){
+    public ProductManagementUseCase getProductManagementUseCase(
+            ProductManagementPersistenceOut productManagementPersistenceOut,
+            ProductManagementCloudinaryOut productManagementCloudinaryOut
+    ){
         return new ProductManagementService(
-            new ProductManagementPersistenceAdapter(this.productManagementRepository),
-            new ProductManagementCloudinaryAdapter()
+            productManagementPersistenceOut,
+            productManagementCloudinaryOut
         );
     }
 
     @Bean
-    public GetProductsUseCase getGetProductsUseCase(){
-        return new GetProductsService(
-            new GetProductsPersistenceAdapter(this.getProductsRepository)
-        );
+    public ProductManagementPersistenceOut getProductManagementPersistenceOut(){
+        return new ProductManagementPersistenceAdapter(this.productManagementRepository);
     }
 
     @Bean
-    public GetProductUseCase getGetProductUseCase(){
-        return new GetProductService(
-            new GetProductPersistenceAdapter(this.getProductRepository)
-        );
+    public ProductManagementCloudinaryOut getProductManagementCloudinaryOut(){
+        return new ProductManagementCloudinaryAdapter();
+    }
+
+    @Bean
+    public GetProductsUseCase getGetProductsUseCase(GetProductsPersistenceOut getProductsPersistenceOut){
+        return new GetProductsService(getProductsPersistenceOut);
+    }
+
+    @Bean
+    public GetProductsPersistenceOut getGetProductsPersistenceOut(){
+        return new GetProductsPersistenceAdapter(this.getProductsRepository);
+    }
+
+    @Bean
+    public GetProductUseCase getGetProductUseCase(GetProductPersistenceOut getProductPersistenceOut){
+        return new GetProductService(getProductPersistenceOut);
+    }
+
+    @Bean
+    public GetProductPersistenceOut getGetProductPersistenceOut(){
+        return new GetProductPersistenceAdapter(this.getProductRepository);
     }
 }
