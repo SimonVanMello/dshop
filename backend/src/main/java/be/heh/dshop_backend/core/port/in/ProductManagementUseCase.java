4b10a539d8e0d3368e3ae942d9051b8f002e5801
Antoine Florent <florentantoine32@gmail.com>
2024-01-24 16:43:57 +0100

    test: modifyProduct test on productManagementService

--- backend/src/main/java/be/heh/dshop_backend/core/port/in/ProductManagementUseCase.java
+++ backend/src/main/java/be/heh/dshop_backend/core/port/in/ProductManagementUseCase.java
@@ -4,6 +4,6 @@ import be.heh.dshop_backend.core.domain.model.Product;
 
 public interface ProductManagementUseCase {
     Product addProduct(ProductManagementAddCommand command);
-    void modifyProduct(ProductManagementModifyCommand command) throws Exception;
+    Product modifyProduct(ProductManagementModifyCommand command) throws Exception;
     void removeProduct(ProductManagementRemoveCommand command);
 }
